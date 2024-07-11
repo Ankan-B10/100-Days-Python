a94144f972303c3d8a69e1422fb8ee4e5f1f5612
Ankan Bera <beraankan8@gmail.com>
2024-07-11 11:51:02 +0530

    if else elif

---
+++ 100 Days/Day14/if_else.py
@@ -0,0 +1,22 @@
+a = int(input("Enter your age :"))
+print("Ypur age is :",a)
+#condition
+if(a>18):
+    print("You can Drive")
+else:
+    print("You can't drive")
+
+print("Yay!")
+
+# Conditional Operator-> {==,!=,>,<,>=,<=}
+print(a>18)
+print(a<18)
+print(a==18)
+print(a!=18)
+
+applePrice = 210
+budget = 200
+if (applePrice <= budget):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
---
+++ 100 Days/Day14/nested_if.py
@@ -0,0 +1,14 @@
+num = 18
+if (num < 0):
+    print("Number is negative.")
+elif(num > 0):
+    if(num <= 10):
+        print("Num is between 1-10")
+    elif(num > 10 and num <= 20):
+        print("Num is between 11-20")
+    else:
+        print("Num is greater than 20")
+else:
+    print("Number is zero")
+
+print("Hahaha Bye!!!")
