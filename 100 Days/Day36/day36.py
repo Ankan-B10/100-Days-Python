0bfe68e9f2cb6f0e346ebac6808897dab93945a2
Ankan Bera <beraankan8@gmail.com>
2024-10-09 08:09:32 +0530

    Day 36 done

---
+++ 100 Days/Day36/day36.py
@@ -0,0 +1,28 @@
+a = input("Enter the number: ")
+print(f"Multiplication table of {a} is: ")
+
+try:
+    for i in range(1, 11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+except Exception as e:
+    print("Invalid Input")
+
+print("End of program")
+    
+## Example:
+# try:
+#     num = int(input("Enter an integer: "))
+# except ValueError:
+#     print("Number entered is not an integer.")
+
+
+## Multiple Exception
+try:
+    num = int(input("Enter an integer: "))
+    a = [6, 3]
+    print(a[num])
+except ValueError:
+    print("Number entered is not an integer.")
+
+except IndexError:
+    print("Index Error Occurs")
