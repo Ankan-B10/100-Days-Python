4f34fd85c31f8b379aeae12398a61b5e307e7bd3
Ankan Bera <beraankan8@gmail.com>
2024-10-23 03:16:12 +0530

    update code

--- 100 Days/day45.py
+++ 100 Days/day45.py
@@ -1,1 +1,10 @@
 ## `if "__name__ == "__main__"` in Python
+
+import ankan # also print welcome function
+
+ankan.welcome()
+
+#idiom is a common pattern used in Python scripts to determine whether the script is being run directly or being imported as a module into another script.
+
+#If you run this script directly, it will output "Running script directly".
+
